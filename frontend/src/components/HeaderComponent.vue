0ca51999078bc04fada02fe0b27e894fa5aa496f
Erol-web <erol.mehmed@abv.bg>
2023-09-22 01:17:26 +0300

    e-commerce: refactoring

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -12,7 +12,12 @@ const switchCategoryEmit = (categoryIndex: number) => {
 
 <template>
   <header class="sticky-top">
-      <h1>Nerd's Nest</h1>
+    <router-link class="name" to="/">Nerd's Nest</router-link>
+    <div class="second-group">
+      <router-link class="links" to="/messages">Messages</router-link>
+      <router-link class="links" to="/my">Your Account</router-link>
+      <router-link class="links" to="/messages">Add ad</router-link>
+    </div>
   </header>
 </template>
 
@@ -20,15 +25,34 @@ const switchCategoryEmit = (categoryIndex: number) => {
 .sticky-top {
   display: flex;
   height: 80px;
+  justify-content: space-between;
   align-items: center;
   border-bottom: 2px solid black;
   background: var(--main-color);
   color: var(--vt-c-white);
   padding: 0 280px;
 
-  h1 {
+  a {
+    text-decoration: none;
+    color: var(--vt-c-white);
+    background: none;
+  }
+
+  .name {
     font-size: 22px;
-    margin: 0;
+
+    &:hover {
+      color: rgb(228, 255, 255);
+    }
+  }
+  .second-group {
+    .links {
+      font-size: 16px;
+      
+      &:nth-of-type(-n+2) {
+        margin-right: 10px;
+      }
+    }
   }
 }
 </style>
