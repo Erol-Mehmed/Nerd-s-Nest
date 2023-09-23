0681e1547a156f66a4c73473d203fb9adfb95e91
Erol-web <erol.mehmed@abv.bg>
2023-09-23 18:07:04 +0300

    backend start

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,20 @@
 const express = require('express');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'dbuser',
+  password: '123456',
+  database: 'my_db'  
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, field) => {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
 const app = express();
 
 app.listen(3000, () => {
